3d24a9233fb1d5ede4fe533b1802420d8ebce87c
isapsz <42344186+isapsz@users.noreply.github.com>
2020-05-03 14:40:40 -0300

    Primeiros arquivos para teste para conexão com banco de dados

---
+++ dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class dashboard extends StatelessWidget{
+  String url = "http://192.168.15.5:3000/";
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
---
+++ main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _counter = "";
+
+  void _incrementCounter() {
+    setState(() {
+      getApi();
+    });
+  }
+
+  Future getApi() async
+  {
+    http.Response  response = await http.get(
+        Uri.encodeFull("http://192.168.15.5:3000/usuario"),
+        headers:
+        {
+          "Accept": "Aplication/json"
+        }
+    );
+    _counter = json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
